7c06c21075de60527e5d9c1e392e450d4b0004b9
Gove <150231701+Gove2004@users.noreply.github.com>
2024-12-22 17:25:57 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
+    import('@tailwindcss/typography'),
+    import('@tailwindcss/forms'),
   ],
 };
